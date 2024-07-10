18eaa247db7495f7ea911ee1768545a6e3d53dc1
Shaan Hurley <shaan@Shaans-MacBook-Pro.local>
2024-07-09 17:19:30 -0700

    analysis now updates automatically on save

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,7 @@
 import { analyze } from "@/utils/ai"
 import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import { revalidatePath } from "next/cache"
 import { NextResponse } from "next/server"
 
 export const PATCH = async (request: Request, { params }) => {
@@ -32,6 +33,7 @@ export const PATCH = async (request: Request, { params }) => {
         update: analysis,
     })
 
+
     console.log(updated)
-    return NextResponse.json({ data: updatedEntry })
+    return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
